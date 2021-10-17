1612c5da017d708edb71f5756219c2e7250fbf78
rhian-cs <rhian.luis.cs+github@gmail.com>
2021-10-17 00:11:49 -0300

    Move generic Window logic from MainWindow to DefaultWindow

---
+++ gui/DefaultWindow.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class DefaultWindow extends JFrame {
+  private static final int WINDOW_WIDTH = 480;
+  private static final int WINDOW_HEIGHT = 240;
+
+  public DefaultWindow(String title, JPanel contentPane) {
+    super(title);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    setContentPane(contentPane);
+    setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    setLocationRelativeTo(null);
+    setVisible(true);
+  }
+}
--- gui/MainWindow.java
+++ gui/MainWindow.java
@@ -1,19 +1,7 @@
 package gui;
 
-import javax.swing.JFrame;
-
-public class MainWindow extends JFrame {
-  private static final String TITLE = "Hash Calculator";
-  private static final int WINDOW_WIDTH = 480;
-  private static final int WINDOW_HEIGHT = 240;
-
+public class MainWindow extends DefaultWindow {
   public MainWindow() {
-    super(TITLE);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    setContentPane(new MainPanel());
-    setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-    setLocationRelativeTo(null);
-    setVisible(true);
+    super("Hash Calculator", new MainPanel());
   }
 }
