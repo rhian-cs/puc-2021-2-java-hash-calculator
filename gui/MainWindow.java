083ebdbcab037b2bedfa479d78e5483467da33c5
rhian-cs <rhian.luis.cs+github@gmail.com>
2021-10-17 00:01:10 -0300

    Remove native look and feel because file selector looks weird on Linux

--- gui/MainWindow.java
+++ gui/MainWindow.java
@@ -1,7 +1,6 @@
 package gui;
 
 import javax.swing.JFrame;
-import javax.swing.UIManager;
 
 public class MainWindow extends JFrame {
   private static final String TITLE = "Hash Calculator";
@@ -11,13 +10,6 @@ public class MainWindow extends JFrame {
   public MainWindow() {
     super(TITLE);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    try {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } catch (Exception e) {
-      System.err.println("An error occurred when setting the native look and feel.");
-      e.printStackTrace();
-    }
 
     setContentPane(new MainPanel());
     setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
